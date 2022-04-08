353e23fc3d80d72f9779b3104064dffa5ec4dd66
yurii <yurii.levkovich@gmail.com>
2022-04-08 18:09:56 +0300

    f

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-hello
+<?php
+
+$servername = "localhost";
+$database = "mailwizz";
+$username = "root";
+$password = "Cvk9bpk1vV";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+$sql = "SELECT * from mailwizz.mw_list";
+if ($res = mysqli_query($conn, $sql)) {
+echo '<pre>'; print_r([    $res    ]); echo die;
+
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
