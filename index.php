ded5154566599a1d428f387e1741978cf72d0764
yurii <yurii.levkovich@gmail.com>
2022-04-08 22:28:17 +0300

    test create list

--- index.php
+++ index.php
@@ -59,6 +59,14 @@ function getConnect() {
     
     return $conn;
 }
+function runQuery($sql) {
+    $conn = getConnect();
+    if ($result = mysqli_query($conn, $sql)) {
+        echo '<pre>'; print_r([   'success', $result    ]); echo die;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
 
 function createList($data) {
     $list_uid = generateRandomString(13);
@@ -68,9 +76,7 @@ function createList($data) {
             (list_id, list_uid, customer_id, name, display_name, description, visibility, opt_in, opt_out, welcome_email, removable, subscriber_require_approval, subscriber_404_redirect, subscriber_exists_redirect, meta_data, status, date_added, last_updated) VALUES
             (NULL, $list_uid, 1, '$name', '$name', '$name', 'public', 'single', 'single', 'no', 'yes', 'no', '', '', 0x613A323A7B733A33383A2269735F73656C6563745F616C6C5F61745F616374696F6E5F7768656E5F737562736372696265223B693A303B733A34303A2269735F73656C6563745F616C6C5F61745F616374696F6E5F7768656E5F756E737562736372696265223B693A303B7D, 'active', NOW(), NOW());";
 
-    $conn = getConnect();
-    $result = mysqli_query($conn, $sql);
-    echo '<pre>'; print_r([    $result    ]); echo die;
+    echo '<pre>'; print_r([    runQuery($sql)    ]); echo die;
     
 }
 
