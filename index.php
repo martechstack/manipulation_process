82b33988c9574c0aeaeba670ed4edca5ab7427d4
yurii <yurii.levkovich@gmail.com>
2022-04-12 21:12:47 +0300

    test not att

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ function getListIdByUid($list_uid) {
     throw new Exception('Cannot get list id...');
 }
 function getDataAll() {
-    $sql = "SELECT * FROM mailwizz.data_all WHERE Carrier NOT IN ('att') LIMIT 1 OFFSET 0";
+    $sql = "SELECT * FROM mailwizz.data_all WHERE Carrier NOT IN ('att')"; // LIMIT 1 OFFSET 0
     $result = runQuery($sql);
     while ($obj = $result->fetch_object()) {
         $ar[] = $obj;
