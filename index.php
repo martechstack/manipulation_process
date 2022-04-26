4a0c0098354b003a7739a4bfae943463b8009ee8
yurii <yurii.levkovich@gmail.com>
2022-04-26 19:09:02 +0300

    26

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ const CARRIER_VERIZON = 'verizon';
 const CARRIER_TMOBILE = 'tmobile';
 const CARRIER_ATT = 'att';
 
-create('A26_VZ_1000_', 20, CARRIER_VERIZON, 1000, false);
-create('A26_TM_1000_', 20, CARRIER_TMOBILE, 1000, false);
-create('A26_ATT_1000_', 10, CARRIER_ATT, 1000, false);
+//create('A26_VZ_1000_', 20, CARRIER_VERIZON, 1000, false);
+//create('A26_TM_1000_', 20, CARRIER_TMOBILE, 1000, false);
+//create('A26_ATT_1000_', 10, CARRIER_ATT, 1000, false);
 create('A26_VZ_100_', 5, CARRIER_VERIZON, 100, false);
 create('A26_TM_100_', 5, CARRIER_TMOBILE, 100, false);
 create('A26_ATT_100_', 5, CARRIER_ATT, 100, false);
