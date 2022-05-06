50fd57e2d4488756d90d1b8c64de8f36e1115e75
yurii <yurii.levkovich@gmail.com>
2022-05-06 21:52:26 +0300

    tm

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ const CARRIER_TMOBILE = 'tmobile';
 const CARRIER_ATT = 'att';
 
 //create('05MAY_VZ_100_', 20, CARRIER_VERIZON, 100, false);
-create('06_MAY_TM_100_', 10, CARRIER_TMOBILE, 100, false);
+create('06_MAY_TM_1000_', 2, CARRIER_TMOBILE, 1000, false);
 
 function create($listName, $listCount, $carrier, $limit, $random = false) {
     for ($i = 1; $i <= $listCount; $i++) {
